e006f0e23e1d74d1fd7d5fddbd318b334b40d21d
bambooozeld <77414753+bambooozeld@users.noreply.github.com>
2022-02-25 00:26:57 +0100

    PogU

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
 const fs = require('fs');
 require("dotenv").config();
-client.commands = new Collection();
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN);
 const { Client, Intents, MessageSelectMenu, Collection } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+client.commands = new Collection();
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-const commands = [{
-  name: 'pingtest', description: 'Replies with Pong!',
-  name: 'serverinfo', description: 'Returns Server Info',
-  name: 'userinfo', description: 'Returns Info about the users User'
-}];
-
-
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
 
 (async () => {
   try {
@@ -33,27 +38,19 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	// Set a new item in the Collection
-	// With the key as the command name and the value as the exported module
-	client.commands.set(command.data.name, command);
-}
-const commands = [];
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
 
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	commands.push(command.data.toJSON());
-}
+  const command = client.commands.get(interaction.commandName);
 
-const command = client.commands.get(interaction.commandName);
-if (!command) return;
-try {
-  await command.execute(interaction);
-} catch (error) {
-  console.error(error);
-  await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-}
+  if (!command) return;
+
+  try {
+      await command.execute(interaction);
+  } catch (error) {
+      console.error(error);
+      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+  }
+});
 
 client.login(process.env.DISCORD_TOKEN);
